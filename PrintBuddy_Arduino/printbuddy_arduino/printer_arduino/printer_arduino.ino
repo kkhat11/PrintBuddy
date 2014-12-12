908d27f0dcd54e967a40459d9a5acb3ce4c6599b
rzav <rzav@users.noreply.github.com>
2014-12-12 09:03:36 -0600

    Update printer_arduino.ino

--- PrintBuddy_Arduino/printbuddy_arduino/printer_arduino/printer_arduino.ino
+++ PrintBuddy_Arduino/printbuddy_arduino/printer_arduino/printer_arduino.ino
@@ -3,45 +3,33 @@ String input;
 
 //Motor Pins
 //Pulley System
-int leftMotorOne = 12;
-int leftMotorTwo = 11;
-
-int rightMotorOne = 10;
-int rightMotorTwo = 9;
+int leftMotor = 10;
+int rightMotor = 11;
 
 //Dobber
-int dobberMotorOne = 7;
-int dobberMotorTwo = 6;
+int dobberMotor = 7;
 
 
 int startBit = 0;
 int endBit = 23;
 
+int positionPrinter = 0;
+
 
 
 void setup() {
+  pinMode(rightMotor, OUTPUT);
+  pinMode(leftMotor, OUTPUT);
+  pinMode(dobberMotor, OUTPUT);
+  digitalWrite(dobberMotor, HIGH);
     
-    pinMode(leftMotorOne, OUTPUT);
-    pinMode(leftMotorTwo, OUTPUT);
-    
-    pinMode(rightMotorOne, OUTPUT);
-    pinMode(rightMotorTwo, OUTPUT);
-    
-    pinMode(dobberMotorOne, OUTPUT);
-    pinMode(dobberMotorTwo, OUTPUT);
-    
-    digitalWrite(dobberMotorOne, HIGH);
-    
-    //digitalWrite(dobberMotorOne, HIGH);
-    //digitalWrite(dobberMotorTwo, LOW);
-    
-    Serial.begin(9600); // Default connection rate for my BT module
+  Serial.begin(9600); // Default connection rate for my BT module
     
 }
 void loop(){
   input = Serial.readStringUntil('\n');
     if(input != ""){
-      while(endBit <= 23)
+      for(int i = 0; i < 1; i++)
       {
         String passedValue = input.substring(startBit, endBit);
         beginPrintLine(passedValue);
@@ -49,65 +37,55 @@ void loop(){
         endBit += 24;
         //Move printbuddy
       }
-      startBit = 0;
-      endBit = 23;
     }
   }
 void beginPrintLine(String input) {
-  for(int i = 0; i < input.length(); i++){
+  for(int i = 0; i <= 23; i++){
+    positionPrinter++;
     if(input.charAt(i) == '1'){
-       
-      delay(1000); 
-      dropDobber();
-       /*dobberMove();
-       delay(1000);*/
+       dropDobber();
+       delay(750);
+       moveChassis();
+       delay(50);
     }
     else
     {
-      //delay(2000); 
-      //moveChassis();
-       //delay(1000);
+      delay(1500); 
+      moveChassis();
+      delay(50);
+       
     }
   }
+  positionPrinter = 0;
   rewind();
-}
+ }
+
 
 void rewind(){
-  digitalWrite(leftMotorOne, HIGH);
-  digitalWrite(leftMotorTwo, LOW);
-  delay(2000);
-  digitalWrite(leftMotorOne, LOW);
-  digitalWrite(leftMotorTwo, LOW);
-  delay(1000);
+  digitalWrite(leftMotor, HIGH);
+  delay(4000);
+  digitalWrite(leftMotor, LOW);
   
 }
 
 void dropDobber(){
-    digitalWrite(dobberMotorOne, LOW);
-    delay(2000);
-    digitalWrite(dobberMotorOne, HIGH);
+    digitalWrite(dobberMotor, LOW);
+    delay(750);
+    digitalWrite(dobberMotor, HIGH);
       
 }
 
-void dobberMove() {
-  digitalWrite(leftMotorOne, LOW);
-  digitalWrite(leftMotorTwo, LOW);
-  
-  digitalWrite(rightMotorOne, HIGH);
-  digitalWrite(rightMotorTwo, LOW);
-  delay(400);
-  digitalWrite(rightMotorOne,LOW);
-  digitalWrite(rightMotorTwo,LOW);
-}
-
 void moveChassis() {
-  digitalWrite(leftMotorOne, LOW);
-  digitalWrite(leftMotorTwo, LOW);
+  digitalWrite(rightMotor, HIGH);
   
-  digitalWrite(rightMotorOne, HIGH);
-  digitalWrite(rightMotorTwo, LOW);
-  delay(450);
-  digitalWrite(rightMotorOne,LOW);
-  digitalWrite(rightMotorTwo,LOW);
+  if(positionPrinter >= 6){
+    int timerDelay = (((positionPrinter / 6) + 1) * 100);
+    delay(timerDelay);
+  }
+  else
+  {
+   delay(150); 
+  }
+  digitalWrite(rightMotor, LOW);
+  delay(300);
 }
-
